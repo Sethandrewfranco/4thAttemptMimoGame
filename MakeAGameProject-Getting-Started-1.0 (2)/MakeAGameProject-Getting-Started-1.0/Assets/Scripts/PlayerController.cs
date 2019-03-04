759e48124430f37db6b7b3f730dc28c01add6d53
Sethandrewfranco <43960700+Sethandrewfranco@users.noreply.github.com>
2019-03-03 21:00:46 -0500

    Writting the Script

--- MakeAGameProject-Getting-Started-1.0 (2)/MakeAGameProject-Getting-Started-1.0/Assets/Scripts/PlayerController.cs
+++ MakeAGameProject-Getting-Started-1.0 (2)/MakeAGameProject-Getting-Started-1.0/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        var rigidBody = GetComponent<Rigidbody2D>();
+        var transform = GetComponent<Transform>();
+        if (Input.GetKey("right"))
+        {
+            rigidBody.velocity = new Vector2(5, rigidBody.velocity.y);
+        }
+        if (Input.GetKey("left"))
+        {
+            rigidBody.velocity = new Vector2(-5, rigidBody.velocity.y);
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, 10);
+        }
+        if (transform.position.y < -6)
+        {
+            if (transform.position.x < 2)
+            {
+                transform.position = new Vector2(-5, 2);
+            }
+            else
+            {
+                transform.position = new Vector2(2, 2);
+            }
+        }
+    }
 }
